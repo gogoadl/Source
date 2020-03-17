eab9468edcfad2ff7811590752b6f20a3aae5419
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-03-17 23:01:19 +0900

    Create FileUploadUtils.java

---
+++ FileUploadUtils.java
@@ -0,0 +1,43 @@
+package com.example.littlesight.ui.main;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class FileUploadUtils{
+    public static void sendToServer(File file,String userID){
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("files", file.getName(), RequestBody.create(MultipartBody.FORM, file))
+                .addFormDataPart("user_Email",userID)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://seoon53.cafe24.com/update_image.php")
+                .post(requestBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TEST : ", response.body().string());
+
+            }
+        });
+    }
+
+}
